05871dd7351ec4b8cbc929e0fba5cc55b768c28e
Maria Masljonok <maria@onoffapp.com>
2021-01-31 17:17:51 +0200

    Initial Commit

---
+++ UISliderHomeWork2.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UISliderHomeWork2.2
+//
+//  Created by Maria Masljonok on 31.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
